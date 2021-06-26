f007056d8b578e1cf616bf0d93e495c6fa97eb4d
namnguyen1980 <69713125+namnguyen1980@users.noreply.github.com>
2021-06-26 21:09:35 +0700

    upload login tests (#3)
    Co-authored-by: Nam Nguyen <nanguyen@confluence.com>

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -6,23 +6,40 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './src/**/*.spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
   },
+  /*
+  multiCapabilities: [
+    {
+        "browserName": "chrome",        
+        shardTestFiles: true,
+        maxInstances: 2,
+        specs: ['studentlist.spec.js', 'login.spec.js']
+    },
+    {
+        "browserName": "firefox",
+        "count": 1,
+        specs: ['login.spec.js']
+    },
+],
+*/
+
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 50000,
     print: function() {}
   },
   onPrepare() {
+    browser.driver.manage().window().maximize();
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+  },
 };
---
+++ e2e/src/UITests/Tests/Login/login.spec.ts
@@ -0,0 +1,83 @@
+import { ExpectedConditions, browser } from 'protractor';
+import { LoginPage } from '../../Pages/login.page';
+
+
+describe('Test Login page:', function () {
+
+  let until = ExpectedConditions;
+  let loginPage = new LoginPage();
+
+  // parameters
+  let wrongFormatUsername = "abc";
+  let wrongFormatPassword = "123";
+  let invalidUsername = "admin123@yopmail.com";
+  let invalidPassword = "admin123456";
+  let validUsername = "admin@yopmail.com";
+  let validPassword = "admin123";
+
+  // Timing
+  var maxTime: number = 500;
+
+  beforeAll(() => {
+    browser.get('/');
+  });
+
+  afterAll(function () {
+    browser.executeScript('window.sessionStorage.clear();');
+    browser.executeScript('window.localStorage.clear();');
+  });
+
+  describe('When launch the page:', function () {
+    it('should be able to launch successfully', () => {
+      expect(loginPage.getNodeAppRoot().isElementPresent).toBeTruthy();
+    });
+
+    it('should be able to see Login box with Login button disabled', () => {
+      expect(loginPage.getBoxLogin().isElementPresent).toBeTruthy();
+      expect(loginPage.getBtnLogin().getAttribute('disabled')).toEqual('true');;
+    });
+
+  });
+
+  describe('When on Login page, user enters username and password with invalid format:', function () {
+    it('should show a message to ask user enter valid email and password', () => {
+
+      loginPage.setTxtUsername(wrongFormatUsername);
+      loginPage.setTxtPassword(wrongFormatPassword);
+      expect(loginPage.getUsernameErrorFormat().getText()).toEqual('Please enter valid email address');
+      expect(loginPage.getPasswordeErrorFormat().getText()).toEqual('Password must be at least 6 characters long, and contain a number');
+      loginPage.clearTxtUsername();
+      loginPage.clearTxtPassword();
+    });
+  });
+
+  describe('When on Login page, user enters username and password with valid format:', function () {
+    it('should NOT show a message to ask user enter valid email and password', () => {
+      loginPage.setTxtUsername(validUsername);
+      loginPage.setTxtPassword(validPassword);
+      expect(loginPage.isEnterValidEmailErrorExisted());
+      expect(loginPage.isEnterValidPasswordErrorExisted());
+      loginPage.clearTxtUsername();
+      loginPage.clearTxtPassword();
+    });
+  });
+
+  describe('When on Login page, user enters wrong username and/or password:', function () {
+    it('should be able to see a message to notify user enter wrong email and password', () => {
+      loginPage.setTxtUsername(invalidUsername);
+      loginPage.setTxtPassword(invalidPassword);
+      loginPage.getBtnLogin().click().then(function () {
+        expect(loginPage.getInvalidCredential().isElementPresent).toBeTruthy();
+      });
+      loginPage.clearTxtUsername();
+      loginPage.clearTxtPassword();
+    });
+  });
+
+  describe('When on Login page, user enters the correct username and password are entered:', function () {
+    it('should be able to login successfully and go to Student list page', () => {
+      loginPage.logIn();
+      expect(loginPage.getSuccessCredential().isElementPresent).toBeTruthy();
+    });
+  });
+});
